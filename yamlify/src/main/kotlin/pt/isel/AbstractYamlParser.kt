3219d764f3d8797cdafc8bbaf4923b62b5185af2
Initdd <a50468@alunos.isel.pt>
2024-04-01 20:39:58 +0100

    Commented for better understanding

--- yamlify/src/main/kotlin/pt/isel/AbstractYamlParser.kt
+++ yamlify/src/main/kotlin/pt/isel/AbstractYamlParser.kt
@@ -15,117 +15,192 @@ abstract class AbstractYamlParser<T : Any>(private val type: KClass<T>) : YamlPa
     abstract fun newInstance(args: Map<String, Any>): T
 
 
+    // Parse the yaml text into an object
     final override fun parseObject(yaml: Reader): T = newInstance(getObjectValues(yaml))
 
+    // Parse the yaml text into a list of objects
     final override fun parseList(yaml: Reader): List<T> = createObjectsFromList(getListValues(yaml))
 
+    // Creates a list of defined objects from a list of objects
     private fun createObjectsFromList(objectsList: List<Any>): List<T> {
+        // Create a list to hold the objects
         val resultList = mutableListOf<T>()
+        // Iterate over the list of objects
         objectsList.forEach {
+            // Check if the object is a map
             if (it is Map<*, *>) {
+                // If it is a map, create a new instance of the object
                 resultList.add(newInstance(it as Map<String, Any>))
             } else if (it is List<*>) {
+                // If it is a list, execute the function recursively
+                // When it is a nested list
                 resultList.add(createObjectsFromList(it as List<Any>) as T)
             }
         }
         return resultList
     }
 
+    // Get the list of objects from yaml text
     private fun getListValues(yaml: Reader): List<Any> {
+        // Read the text from the reader
+        // this will return a string with the yaml content
         val yamlText = yaml.readText()
+        // Get the list of objects from the yaml text
         val yamlObjects = getObjectList(yamlText)
+        // Create a list to hold the objects
         val resultList = mutableListOf<T>()
+        // And then, iterate over the list of objects
         yamlObjects.forEach {
+            // Check if the object is a list or a single object
             if (getObjectList(it).size > 1) //check for nested lists
+                // If it is a list, recursively call getListValues
+                // to handle the nested list
                 resultList.add(getListValues(it.reader()) as T)
             else
+                // If it is a single object, call getObjectValues
                 resultList.add(getObjectValues(it.reader()) as T)
         }
         return resultList
     }
 
+    // Get the values of the object from the yaml text
     private fun getObjectValues (yaml: Reader): Map<String, Any> {
 
+        // Read the text from the reader
+        // and clean the empty lines
         val lines = yaml.readLines().dropWhile { it.isBlank() }
+        // Create a mutable map to hold the results
         val map = mutableMapOf<String, Any>()
+        // index to iterate over the lines
         var i = 0
 
+        // Get the indentation of the object
+        // In other words, the number of spaces before the first key
         val objIndentation = lines.first().takeWhile { it == ' ' }.length
 
+        // Iterate over the lines
         while (i < lines.size) {
 
+            // Get the current line
             val line = lines[i++]
 
+            // Check if the line is blank
+            // If it is, continue to the next line
             if (line.isBlank()) continue
 
+            // Get the indentation of the current line
             val indentation = line.takeWhile { it == ' ' }.length
 
+            // Check if the indentation is valid
             if (indentation != objIndentation) throw IllegalArgumentException("Invalid indentation at: ${line.trim()}")
 
+            // Split the line by the colon
+            // This returns an array with the key and the value
             val parts = line.split(":").map { it.trim() }.filter { it.isNotBlank() }
 
             when {
+                // When the map already contains the key
+                // Throw an exception
                 map.containsKey(parts[0]) -> throw IllegalArgumentException("Duplicate key ${parts[0]} for ${type.simpleName}")
+                // When the line has two parts (key and value)
+                // Add the key and the value to the map
                 parts.size == 2 -> map[parts[0]] = parts[1]
+                // When the line is a scalar (has a dash)
+                // Add the key and the value to the map
                 isScalar(line) -> map[parts[0]] = line.split("-").last().trim()
+                // When the line is a list
                 else -> {
+                    // Get the next line
+                    // i, because it was incremented before
                     val nextLine = lines[i]
+                    // Get the lines that are indented
                     val indentedLines = getLinesSequence(lines, i, indentation)
 
+                    // Save as the value of the key,
+                    // The result of the recursive call to getObjectValues
                     map[parts[0]] = if (nextLine.contains("-")) {
+                        // If the next line contains a dash, it is a list
                         getListValues(indentedLines.joinToString("\n").reader())
                     } else {
+                        // If the next line does not contain a dash, it is an object
                         getObjectValues(indentedLines.joinToString("\n").reader())
                     }
 
+                    // Increment the index by the number of indented lines
                     i += indentedLines.size
                 }
             }
 
         }
-
-
         return map
     }
 
+    // Check if a line is a scalar
     private fun isScalar(line: String): Boolean {
+        // Conditions: The line must not be blank, must contain a dash, and must not be only a dash
         return line.filter { it != '-' }.isNotBlank() && line.contains("-")
     }
+
+    // Get the lines that are indented
     private fun getLinesSequence(lines: List<String>, start: Int, indentation: Int): List<String> {
+        // Drop the lines before the start index
         return lines.drop(start).takeWhile { line ->
+            // Take the lines while the indentation is greater than the object indentation
             val lineIndentation = line.takeWhile { it == ' ' }.length
             lineIndentation > indentation
         }
     }
+
+    // Get the list of objects from the yaml text
     private fun getObjectList(yaml: String): List<String> {
 
+        // Remove the empty lines from the yaml text
         val lines = yaml.lines().dropWhile { it.isBlank() }
+        // Get the indentation of the object
         val objIndentation = lines.first().takeWhile { it == ' ' }.length
 
+        // Create a mutable list to hold the results
         val list = mutableListOf<String>()
+        // Create a mutable list to hold the current lines
         val currLines = mutableListOf<String>()
 
+        // Iterate over the lines
         lines.forEach { line ->
+            // Check if the line is blank
             if (line.isNotBlank()) {
+                // Get the indentation of the current line
                 val lineIndentation = line.takeWhile { it == ' ' }.length
 
+                // Check if the indentation is valid
+                // In other words, if the difference between the current indentation and the object indentation is even
                 if ((lineIndentation - objIndentation) % 2 != 0)
+                    // If it is not valid, throw an exception
                     throw IllegalArgumentException("Invalid indentation at $line")
 
+                // Check if the line is a scalar
+                // To do this, check if the line contains a dash
                 if (line[objIndentation] == '-') {
+                    // If it is a scalar, add the line to the list
                     if (isScalar(line)) {
                         list.add(line)
+                    // If it is not a scalar
+                    // Save the current lines in the list
+                    // And clear the current lines
                     } else {
                         list.add(currLines.joinToString("\n"))
                         currLines.clear()
                     }
                 } else {
+                    // If the line is not a scalar,
+                    // Add the line to the current lines
                     currLines.add(line)
                 }
             }
         }
 
+        // Add the last set of lines to the list
         list.add(currLines.joinToString("\n"))
+        // End filtering the empty lines
         return list.filter { it.isNotBlank() }
     }
 }
--- yamlify/src/main/kotlin/pt/isel/YamlParserReflect.kt
+++ yamlify/src/main/kotlin/pt/isel/YamlParserReflect.kt
@@ -32,65 +32,87 @@ class YamlParserReflect<T : Any>(private val type: KClass<T>) : AbstractYamlPars
      */
     override fun newInstance(args: Map<String, Any>): T {
 
+        // Get the first constructor of the type (the primary constructor)
         val constructor = type.constructors.first()
 
+        // If the constructor has no parameters, return a new instance of the type
         if (constructor.parameters.isEmpty()) {
             return castValueToType(args.values.first(), type.starProjectedType) as T
         }
 
+        // Map the properties to the constructor parameters
         val properties = args.keys.map { prop ->
             prop to matchParameter(prop, constructor.parameters)
         }
 
+        // Check for duplicate properties
         val duplicateProps = properties.groupBy { it.second }.mapNotNull {
+            // If it's (the property) value has more than one element, it is a duplicate
             if (it.value.size > 1) it
             else null
         }
 
+        // If there are duplicate properties, throw an exception with the names of the properties
         if (duplicateProps.isNotEmpty()) {
             val names = duplicateProps.map { prop -> prop.key!!.name to prop.value.map { it.first } }
             throw IllegalArgumentException("Duplicate properties: $names")
         }
 
+        // Get the constructor arguments
         val constructorArgs = properties.mapNotNull { (srcProp, destProp) ->
             destProp?.let { destProp ->
-                val value = args[srcProp]
-                val type = destProp.type
-                val castedValue = value?.let { castValueToType(it, type) }
+                val value = args[srcProp] // Get the value of the property
+                val type = destProp.type // Get the type of the property
+                val castedValue = value?.let { castValueToType(it, type) } // Cast the value to the suitable type
                 destProp to castedValue
             }
         }
+        // Return a new instance of the type using the constructor with the arguments
         return constructor.callBy(constructorArgs.toMap())
     }
+
+    // Get the first constructor parameter that matches the property
     private fun matchParameter(
+        // The source property name
         srcProp: String,
+        // The constructor parameters list
         ctorParameters: List<KParameter>) : KParameter?{
+        // Return the first parameter that matches the property
         return ctorParameters.firstOrNull { arg ->
+            // Check if the property name is equal to the parameter name
+            // or if the property has a YamlArg annotation with the same name as the parameter
             srcProp == arg.name || arg.annotations.any { it is YamlArg && it.yamlName == srcProp }
         }
     }
+
+    // Cast the given value to the suitable type
     private fun castValueToType(value: Any, type: KType): Any {
         val arg = value as? String
         return when (type.classifier) {
-            String::class -> arg!!
-            Int::class -> arg!!.toInt()
-            Long::class -> arg!!.toLong()
-            List::class -> getIterableValue(value, type)
-            Set::class -> getIterableValue(value, type).toSet()
-            Array::class -> getIterableValue(value, type).toTypedArray()
-            Sequence::class -> getIterableValue(value, type).asSequence()
-            ArrayList::class -> ArrayList(getIterableValue(value, type))
-            else -> yamlParser(type.classifier as KClass<*>).newInstance(value as Map<String, Any>)
+            String::class -> arg!!  // If the type is a string, return the value as is
+            Int::class -> arg!!.toInt() // If the type is an int, return the value as an int
+            Long::class -> arg!!.toLong()   // If the type is a long, return the value as a long
+            List::class -> getIterableValue(value, type)    // If the type is a list, use getIterableValue to get the list
+            Set::class -> getIterableValue(value, type).toSet() // If the type is a set, use getIterableValue to get the set
+            Array::class -> getIterableValue(value, type).toTypedArray()    // If the type is an array, use getIterableValue to get the array
+            Sequence::class -> getIterableValue(value, type).asSequence()   // If the type is a sequence, use getIterableValue to get the sequence
+            ArrayList::class -> ArrayList(getIterableValue(value, type))    // If the type is an ArrayList, use getIterableValue to get the list and create a new ArrayList
+            else -> yamlParser(type.classifier as KClass<*>).newInstance(value as Map<String, Any>) // If the type is a custom type, use the yamlParser to get the instance
         }
     }
     private fun getIterableValue(value: Any, type: KType): List<Any> {
 
+        // Cast the value to a List of Any
         val list = value as List<*>
 
+        // Iterate over the list
         return list.map {
+            // If the type is a list, use getIterableValue to get the list
             if (it is List<*>)
                 getIterableValue(it, type.arguments.first().type!!)
+            // If the type is a not a list
             else
+                // If the type is a custom type, use the yamlParser to get the instance
                 yamlParser(type.arguments.first().type!!.classifier as KClass<*>)
                     .newInstance((it as Map<String, Any>))
         }
