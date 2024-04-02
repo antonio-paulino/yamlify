052231417a1279c7429b63803b878e78eeef7f94
Initdd <a50468@alunos.isel.pt>
2024-04-02 19:25:08 +0100

    Created the YamlConvert annotation. TODO: Check if the out keyword is necessary

--- yamlify/src/main/kotlin/pt/isel/Annotations.kt
+++ yamlify/src/main/kotlin/pt/isel/Annotations.kt
@@ -1,5 +1,11 @@
 package pt.isel
 
+import kotlin.reflect.KClass
+
 @Repeatable
 @Target(AnnotationTarget.VALUE_PARAMETER)
 annotation class YamlArg(val yamlName: String)
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class YamlConvert(val parser: KClass<out YamlParser<*>>)
+
